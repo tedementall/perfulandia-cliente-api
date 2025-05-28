c4726fd38d63345132b8ec3515e07cf5cfcb47e5
Andru Alarcon <idazhdz@gmail.com>
2025-05-27 22:33:32 -0400

    Commit proyecto inicial

---
+++ src/main/java/com/perfulandia/perfulandia_cliente_api/PerfulandiaClienteApiApplication.java
@@ -0,0 +1,13 @@
+package com.perfulandia.perfulandia_cliente_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfulandiaClienteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfulandiaClienteApiApplication.class, args);
+	}
+
+}
